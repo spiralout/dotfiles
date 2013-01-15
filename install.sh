1a518407f66ee65eef3f8fc105bc52200599ad0e
Sean Crystal <seancrystal@gmail.com>
2013-01-14 23:37:04 -0600

    forgot git submodule update

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ DOTFILES=`pwd`
 
 # download submodules
 git submodule init
+git submodule update
 
 # run sub directory install scripts
 for installer in `find $DOTFILES -mindepth 2 -maxdepth 2 -name "install.sh"`; do
